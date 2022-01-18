3cb5144d04f6db4edd3bdb839cccb61838496b61
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-01-18 10:02:44 +0200

    Create Animals table

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@
 CREATE TABLE animals (
     id integer(1),
     animal_name varchar(100),
-    date_of_birth date,
+    date_of_birth date NOT NULL,
     escape_attempts integer,
-    neutered boolean,
-    weight_per_kg decimal
+    neutered boolean NOT NULL,
+    weight_per_kg decimal NOT NULL
 );
