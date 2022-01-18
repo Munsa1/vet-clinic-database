6f84288b682812e443156892488c70657e944f93
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-01-18 21:18:17 +0200

    Enter first entry

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Add Inserts for data into the database */
 
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
-VALUES ('Agumon',  '02/02/2020', '0', 'true', '10.23');
+VALUES ('Agumon',  '2020/02/02', '0', 'true', '10.23');
 
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
 VALUES ('Gabumon', '2018/11/15', '2', 'true', '8');
@@ -11,3 +11,9 @@ VALUES ('Pikachu', '2021/01/17', '1', 'false', '15.04');
 
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
 VALUES ('Devimon', '2017/04/12', '5', 'true', '11');
+
+
+/* Add Inserts for data into the database for query and update milestone */
+
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
+VALUES ('Charmander',  '2020/02', '0', 'false', '11');
