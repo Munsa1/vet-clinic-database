8f0e485b0714a747e3dbad3afd4ccb31dad6d678
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-01-18 10:02:07 +0200

    Create Animals tableINSERT INTO TABLE_NAME (column1, column2, column3,...columnN)

---
+++ schema.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE animals (
+    id integer(1),
+    animal_name varchar(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_per_kg decimal
+);
