4b7a95025effffb8eea018a36790ee606c249235
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-01-20 21:37:17 +0200

    Insert values  table

--- data.sql
+++ data.sql
@@ -35,5 +35,10 @@ INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weig
 VALUES ('Blossom',  '1998/10/13', '3', 'true', '17');
 
 
-
+-- Insert Owners Names and Ages Into Owners Table
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19), ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
 
