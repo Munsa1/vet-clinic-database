581c84efdad3b5d687a963b44c00580315f1b2f3
shoirata <shoira.shakirovna@gmail.com>
2022-01-24 21:50:05 +0400

    Update queries

--- schema.sql
+++ schema.sql
@@ -67,7 +67,3 @@ CREATE TABLE visits (
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
-/* Create index on visits table */
-CREATE INDEX visits_animals_id_idx ON visits(id);
-CREATE INDEX visits_vets_id_idx ON visits(id);
-CREATE INDEX visits_owners_id_idx ON owners(email);
