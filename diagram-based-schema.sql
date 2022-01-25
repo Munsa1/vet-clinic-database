25d916fc8b4cdc394c17547acf590efed199d324
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-01-25 15:19:33 +0200

    Add comment

--- diagram-based-schema.sql
+++ diagram-based-schema.sql
@@ -45,7 +45,7 @@ CREATE TABLE invoices (
  CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 );
 
-
+-- Create join table for medical_histories and treatments table
 CREATE TABLE medical_histories_treaments_invoices (
     medical_history_id int,
     treament_id int,
