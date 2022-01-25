a49456858c339409241c5e3478d32445ddb4e1b3
shoirata <shoira.shakirovna@gmail.com>
2022-01-25 17:11:11 +0400

    Add invoice tables

--- diagram-based-schema.sql
+++ diagram-based-schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE medical_histories(
  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
  admitted_at TIMESTAMP,
  status VARCHAR
+ constraint fk_patient foreign key(patient_id) references patients(id)
 );
 
 
@@ -20,4 +21,26 @@ CREATE TABLE treatments(
  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
  type VARCHAR(50),
  name DATE
+);
+
+-- Create invoice_items table
+CREATE TABLE invoice_items (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ unit_price decimal,
+ quantity int,
+ total_price decimal,
+ invoice_id int,
+ treatment_id int
+ CONSTRAINT fk_invoices FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+ CONSTRAINT fk_treatment FOREIGN KEY(treament_id) REFERENCES treaments(id)
+);
+
+-- Create invoice_items table
+CREATE TABLE invoices (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ total_amount decimal,
+ generated_at timestamp,
+ payed_at timestamp,
+ medical_history_id int,
+ CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 );
